7d915d4339f7412fcb3670ce6a50248148479928
mahmoud <eslamimahmoud98@gmail.com>
2022-10-19 17:50:45 +0330

    Change bottom navigation

--- lib/features/base/base_page.dart
+++ lib/features/base/base_page.dart
@@ -1,5 +1,5 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:prosonic/features/home/home_page.dart';
 import 'package:prosonic/features/settings/settings_page.dart';
@@ -57,40 +57,27 @@ class _BasePageState extends State<BasePage> {
           ),
         ],
       ),
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: AppConstant.black,
-        color: AppConstant.gray,
-        animationDuration: const Duration(milliseconds: 200),
-        animationCurve: Curves.ease,
-        buttonBackgroundColor: AppConstant.red,
-        height: 67,
-        index: 0,
-        items: const <Widget>[
-          Icon(
-            Ionicons.home_outline,
-            size: 29,
-          ),
-          Icon(
-            Ionicons.musical_notes,
-            size: 29,
-          ),
-          Icon(
-            Ionicons.albums,
-            size: 29,
-          ),
-          Icon(
-            Ionicons.shield_checkmark,
-            size: 29,
-          ),
-          Icon(
-            Ionicons.settings,
-            size: 29,
-          ),
+      bottomNavigationBar: SnakeNavigationBar.color(
+        snakeShape: SnakeShape.indicator,
+        showSelectedLabels: true,
+        unselectedItemColor: AppConstant.white,
+        selectedItemColor: AppConstant.white,
+        backgroundColor: AppConstant.gray,
+        snakeViewColor: AppConstant.red.withAlpha(120),
+        currentIndex: selectedPos,
+        onTap: (index) => setState(() => selectedPos = index),
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Ionicons.home_outline), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Ionicons.musical_notes), label: 'Tracks'),
+          BottomNavigationBarItem(
+              icon: Icon(Ionicons.albums), label: 'Packages'),
+          BottomNavigationBarItem(
+              icon: Icon(Ionicons.shield_checkmark), label: 'Services'),
+          BottomNavigationBarItem(
+              icon: Icon(Ionicons.settings), label: 'Profile')
         ],
-        onTap: (index) {
-          setState(() {});
-          selectedPos = index;
-        },
       ),
       body: _pages[selectedPos],
     );
