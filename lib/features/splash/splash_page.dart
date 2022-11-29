239a316fb9a22bb94cb127af2616e5c5fd07a0b5
mahmoud <eslamimahmoud98@gmail.com>
2022-11-29 11:42:12 +0330

    fade and scale animation added to the splash logo

--- lib/features/splash/splash_page.dart
+++ lib/features/splash/splash_page.dart
@@ -2,18 +2,58 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:prosonic/core/constant/constant.dart';
 import 'package:prosonic/core/utils/size_config.dart';
-import 'package:prosonic/features/base/base_page.dart';
 import 'package:prosonic/features/splash/splash_bloc.dart';
 
-class SplashPage extends StatelessWidget {
+import '../base/base_page.dart';
+
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   static const path = "/";
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
+  late AnimationController _fadeAnimationController;
+  late AnimationController _sizeAnimationController;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fadeAnimationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2))
+          ..forward();
+    _sizeAnimationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2))
+          ..forward()
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              BlocProvider.of<SplashBloc>(context).add(AppStarted());
+            }
+          });
+
+    _fadeAnimation =
+        Tween<double>(begin: 0, end: 1).animate(_fadeAnimationController);
+    _scaleAnimation =
+        Tween<double>(begin: .7, end: 1).animate(_fadeAnimationController);
+  }
+
+  @override
+  void dispose() {
+    _fadeAnimationController.dispose();
+    _sizeAnimationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return BlocListener(
-      bloc: BlocProvider.of<SplashBloc>(context)..add(AppStarted()),
+      bloc: BlocProvider.of<SplashBloc>(context),
       listener: (context, state) {
         if (state is NavigateToHome) {
           Navigator.pushReplacementNamed(context, BasePage.path);
@@ -25,9 +65,15 @@ class SplashPage extends StatelessWidget {
           child: Hero(
             tag: AppConstant.splashIconHero,
             child: Center(
-              child: Image.asset(
-                "assets/images/p.png",
-                width: SizeConfig.widthMultiplier * 50,
+              child: FadeTransition(
+                opacity: _fadeAnimation,
+                child: ScaleTransition(
+                  scale: _scaleAnimation,
+                  child: Image.asset(
+                    "assets/images/p.png",
+                    width: SizeConfig.widthMultiplier * 50,
+                  ),
+                ),
               ),
             ),
           ),
